428f29d1261963d340b4b748a576d699877a98e0
Egbuleze Francis <francisegbuleze@gmail.com>
2025-01-28 20:50:50 +0100

    second

--- backend/index.js
+++ backend/index.js
@@ -20,11 +20,16 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	// app.use(express.static(path.join(__dirname, "frontend/dist")));
+    const frontendPath = path.join(__dirname, 'frontend', 'dist');
+    app.use(express.static(frontendPath));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+	// app.get("*", (req, res) => {
+	// 	res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	// });
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(frontendPath, 'index.html'));
+    })
 }
 
 app.listen(PORT, () => {
